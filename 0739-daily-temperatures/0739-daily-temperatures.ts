99e5038804f1ebeb569328198f0290d4310b04a8
EustaceCheng <fbi0258zzz@gmail.com>
2023-06-11 14:39:01 +0800

    Time: 274 ms (82.97%), Space: 63 MB (99.03%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.ts
+++ 0739-daily-temperatures/0739-daily-temperatures.ts
@@ -2,18 +2,20 @@ function dailyTemperatures(temperatures: number[]): number[] {
     const n: number = temperatures.length;
     const answer: number[] = new Array(n).fill(0);
 
-    for (let i: number = n - 1, hottest = 0; i >= 0; i--) {
-      const t: number = temperatures[i];
-      if (t >= hottest) {
-        hottest = t;
-        continue;
-      }
 
-      let days: number = 1;
-      while (temperatures[i + days] <= t) {
-        days += answer[i + days];
-      }
-      answer[i] = days;
+    for (let i: number = n - 1,hottest = 0; i >= 0; i--) {
+        const cur: number = temperatures[i];
+        if (cur >= hottest) {
+            hottest = cur;
+            continue;
+        }
+
+        let days: number = 1;
+
+        while (temperatures[ i + days] <= cur) {
+            days += answer[ i + days];
+        }
+        answer[i] = days;
     }
 
     return answer;
