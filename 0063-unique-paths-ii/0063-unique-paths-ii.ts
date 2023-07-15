739a71cf62f70e336bb1f607814728c9254818e8
EustaceCheng <fbi0258zzz@gmail.com>
2023-07-15 15:08:46 +0800

    Time: 46 ms (100.00%), Space: 44.6 MB (67.41%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.ts
@@ -0,0 +1,53 @@
+function uniquePathsWithObstacles(obstacleGrid: number[][]): number {
+    const m = obstacleGrid.length
+    const n = obstacleGrid[0].length
+
+
+
+    if (obstacleGrid[m - 1][n - 1] === 1 || obstacleGrid[0][0] === 1)
+        return 0
+
+    let map = []
+
+    for (let i = 0; i < m; i++) {
+        let row = [];
+        for (let j = 0; j < n; j++) {
+            row.push(0);
+        }
+        map.push(row);
+    }
+
+
+
+    for (let i = 0; i < m; i++) {
+        map[i][0] = 1
+
+        if (obstacleGrid[i][0] === 1) {
+            map[i][0] = 0
+            break;
+        }
+
+    }
+     for (let i = 0; i < n; i++) {
+        map[0][i] = 1
+
+        if (obstacleGrid[0][i] === 1) {
+            map[0][i] = 0
+            break;
+        }
+
+    }
+
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i !== 0 && j !== 0 && !(obstacleGrid[i][j] === 1))
+                map[i][j] = map[i - 1][j] + map[i][j - 1]
+        }
+    }
+
+
+    return map[m - 1][n - 1]
+
+
+};
