931eb7cdec07f96901efc89013e6e88ba7bc9dc8
EustaceCheng <fbi0258zzz@gmail.com>
2023-07-24 16:24:07 +0800

    Time: 74 ms (52.35%), Space: 45.3 MB (55.34%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.ts
+++ 0101-symmetric-tree/0101-symmetric-tree.ts
@@ -1,3 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+
+
 const dfs = (l: TreeNode | null, r: TreeNode | null) => {
   if (!l && !r) return true;
   if (!l || !r) return false;
