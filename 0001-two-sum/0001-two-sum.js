ff7591f2a51c28095e1c1abbe9f45b15c40782f1
parker cheng <fbi0258zzz@gmail.com>
2023-11-28 19:13:04 +0800

    Update 0001-two-sum.js

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,9 +1,9 @@
-var twoSum = function(nums, target) {
-    for(var i=0; i<nums.length-1; i++){
-        for(var j=i+1;j<nums.length;j++){
-            if(target == nums[i]+nums[j]){
-                return [i,j];            
-            }
-        }
-    }     
+var twoSum = function (nums, target) {
+  for (var i = 0; i < nums.length - 1; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (target == nums[i] + nums[j]) {
+        return [i, j]; // test
+      }
+    }
+  }
 };
